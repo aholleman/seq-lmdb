341e9c828ff64f0d6ba9f93f35bdcc87c4158ce1
Alex Kotlar <akotlar@node03.local>
2016-06-19 20:17:49 -0400

    showing that length is faster than index

---
+++ bench/split_list_context.pl
@@ -0,0 +1,20 @@
+use Benchmark 'timethese';
+use strict;
+use warnings;
+my $line = "1\tC\t3000\tA\t5\t6";
+my $chr = 1;
+my $chrLength = length($chr);
+
+sub listContext    { my (undef, undef, $length, undef, undef, $score) = split "\t", $line; }
+sub array       { my @out = split "\t", $line;  }
+sub justChr       { my $chr = substr($line, 0, index($line, "\t") );  }
+sub justChrWithLength       { my $chr = substr($line, 0, $chrLength);  }
+
+timethese (4_000_000, {
+  listContext => \&listContext,
+  array => \&array,
+  justChr => \&justChr,
+  justChrWithLength => \&justChrWithLength,
+});
+
+__END__
