20d7fabbf1e96fc2feb3464c85b542348020456b
Alex Kotlar <alex.kotlar@emory.edu>
2016-10-05 01:18:46 +0000

    playing with optimizing Gene.pm, by not copying arguments on sub call

--- lib/Seq/Tracks/Gene.pm
+++ lib/Seq/Tracks/Gene.pm
@@ -1,7 +1,6 @@
 use 5.10.0;
 use strict;
 use warnings;
-# TODO: Think about allowing 3-deep hash
 package Seq::Tracks::Gene;
 
 our $VERSION = '0.001';
@@ -33,14 +32,8 @@ use Seq::Tracks::Gene::Site::CodonMap;
 use Seq::Tracks::Gene::Definition;
 use Seq::DBManager;
 
-### objects that get used by multiple subs, but shouldn't be public attributes ###
-# All of these instantiated classes cannot be configured at instantiation time
-# so safe to use in static context
-state $siteUnpacker = Seq::Tracks::Gene::Site->new();
-state $siteTypeMap = Seq::Tracks::Gene::Site::SiteTypeMap->new();
-state $codonMap = Seq::Tracks::Gene::Site::CodonMap->new();
-
-### Additional "features" that we will add to our output ###
+########### @Public attributes##########
+########### Additional "features" that we will add to our output ##############
 ### Users may configure these ####
 
 # These are features defined by Gene::Site, but we name them in Seq::Tracks::Gene
@@ -56,7 +49,14 @@ has newCodonKey => (is => 'ro', default => 'alleleCodon');
 has newAminoAcidKey => (is => 'ro', default => 'alleleAminoAcid');
 has exonicAlleleFunctionKey => (is => 'ro', default => 'exonicAlleleFunction');
 
-################################ Private ######################################
+################################ Private Attributes ######################################
+### objects that get used by multiple subs, but shouldn't be public attributes ###
+# All of these instantiated classes cannot be configured at instantiation time
+# so safe to use in static context
+state $siteUnpacker = Seq::Tracks::Gene::Site->new();
+state $siteTypeMap = Seq::Tracks::Gene::Site::SiteTypeMap->new();
+state $codonMap = Seq::Tracks::Gene::Site::CodonMap->new();
+
 ########## The names of various features. These cannot be configured ##########
 ### Positions that aren't covered by a refSeq record are intergenic ###
 state $intergenic = 'intergenic';
@@ -105,6 +105,11 @@ sub BUILD {
   $self->{_exonicAlleleFunctionKey} = $self->exonicAlleleFunctionKey;
   $self->{_codonSequenceKey} = $self->codonSequenceKey;
 
+  # Avoid Accessor performance hit, these can be called many millions of times
+  # in get()
+  $self->{_features} = $self->features;
+  $self->{_joinTrackFeatures} = $self->joinTrackFeatures;
+
   # Not including the txNumberKey;  this is separate from the annotations, which is 
   # what these keys represent
   
@@ -114,9 +119,12 @@ sub BUILD {
 
   $self->{_db} = Seq::DBManager->new();
 
-  # 1 to prepend
-  $self->addFeaturesToHeader([$self->siteTypeKey, $self->{_exonicAlleleFunctionKey}, $self->codonSequenceKey,
-    $self->{_newCodonKey}, $self->{_refAminoAcidKey}, $self->{_newAminoAcidKey}, $self->codonPositionKey,
+  #  Prepend some internal seqant features
+  #  Providing 1 as the last argument means "prepend" instead of append
+  #  So these features will come before any other refSeq.* features
+  $self->addFeaturesToHeader([$self->siteTypeKey, $self->exonicAlleleFunctionKey,
+    $self->codonSequenceKey, $self->newCodonKey, $self->refAminoAcidKey,
+    $self->newAminoAcidKey, $self->codonPositionKey,
     $self->codonNumberKey, $self->strandKey], $self->name, 1);
 
   if($self->hasNearest) {
@@ -136,7 +144,9 @@ sub BUILD {
   if($self->join) {
     my $joinTrackName = $self->joinTrackName;
 
-    $self->addFeaturesToHeader( [ map { "$joinTrackName.$_" } @{$self->joinTrackFeatures} ], $self->name);
+    # Faster to access the hash directly than the accessor
+    $self->addFeaturesToHeader( [ map { "$joinTrackName.$_" } @{$self->{_joinTrackFeatures}} ],
+      $self->name);
 
     # TODO: ould theoretically be overwritten by line 114
     #the features specified in the region database which we want for nearest gene records
@@ -146,7 +156,7 @@ sub BUILD {
     }
   }
 
-  for my $fName ($self->allFeatureNames) {
+  for my $fName (@{$self->{_features}}) {
     $self->{_allCachedDbNames}{$fName} = $self->getFieldDbName($fName);
   }
 };
@@ -157,16 +167,24 @@ sub BUILD {
 # that are found in the user's experiment, for this position that we're annotating
 #@param <Number> $dbPosition : The 0-index position of the current data
 sub get {
-  my ($self, $href, $chr, $dbPosition, $refBase, $allelesAref) = @_;
-
+  #These are the arguments passed to this function
+  #This function may be called millions of times. To speed up access,
+  #Avoid copying the data during sub call
+  #my ($self, $href, $chr, $dbPosition, $refBase, $allelesAref) = @_;
+  #    $_[0]  $_[1]  $_[2]  $_[3]       $_[4]     $_[5]
+  
   my %out;
 
   # Cached field names to make things easier to read
-  my $cachedDbNames = $self->{_allCachedDbNames};
+  # Reads:            $self->{_allCachedDbNames};
+  my $cachedDbNames = $_[0]->{_allCachedDbNames};
 
   ################# Cache track's region data ##############
-  if(!defined $self->{_geneTrackRegionHref}{$chr} ) {
-    $self->{_geneTrackRegionHref}{$chr} = $self->{_db}->dbReadAll( $self->regionTrackPath($chr) );
+  #Reads:     $self->{_geneTrackRegionHref}{$chr} ) {
+  if(!defined $_[0]->{_geneTrackRegionHref}{$_[2]} ) {
+    #Reads:
+    #$self->{_geneTrackRegionHref}{$chr} = $self->{_db}->dbReadAll( $self->regionTrackPath($chr) );
+    $_[0]->{_geneTrackRegionHref}{$_[2]} = $_[0]->{_db}->dbReadAll( $_[0]->regionTrackPath($_[2]) );
   }
   
   ####### Get all transcript numbers, and site data for this position #########
@@ -174,73 +192,93 @@ sub get {
   #<ArrayRef> $unpackedSites ; <ArrayRef|Int> $txNumbers
   my ($siteData, $txNumbers, $multiple);
 
-  # is an <ArrayRef>, where every other element is siteData
-  if( $href->[$self->dbName] ) {
-    ($txNumbers, $siteData) = $siteUnpacker->unpack($href->[$self->dbName]);
+  #Reads:
+  # ( $href->[$self->dbName] ) {
+  if( $_[1]->[$_[0]->dbName] ) {
+    #Reads:                   $siteUnpacker->unpack($href->[$self->dbName]);
+    ($txNumbers, $siteData) = $siteUnpacker->unpack($_[1]->[$_[0]->dbName]);
     $multiple = !! ref $txNumbers;
   }
 
   # ################# Populate nearestGeneSubTrackName ##############
-  if($self->hasNearest) {
+  # Reads:
+  #  $self->hasNearest
+  if($_[0]->hasNearest) {
 
     # Nearest genes are sub tracks, stored under their own key, based on $self->name
     # <Int|ArrayRef[Int]>
     # If we're in a gene, we won't have a nearest gene reference
-    my $nearestGeneNumber = $txNumbers || $href->[$cachedDbNames->{$self->nearestTrackName}];
+    # Reads: =              $txNumbers || $href->[$cachedDbNames->{$self->nearestTrackName}];
+    my $nearestGeneNumber = $txNumbers || $_[1]->[$cachedDbNames->{$_[0]->nearestTrackName}];
 
     if($nearestGeneNumber) {
       for my $geneRef ( ref $nearestGeneNumber ? @$nearestGeneNumber : $nearestGeneNumber ) {
-          for my $nFeature ($self->allNearestFeatureNames) {
-            push @{ $out{ $self->{_allNearestFieldNames}{$nFeature} } },
-              $self->{_geneTrackRegionHref}{$chr}{$geneRef}{ $cachedDbNames->{$nFeature} };
+          # Reads:         ($self->allNearestFeatureNames) {
+          for my $nFeature ($_[0]->allNearestFeatureNames) {
+            #Reads:
+            #push @{ $out{ $self->{_allNearestFieldNames}{$nFeature} } }, 
+            push @{ $out{ $_[0]->{_allNearestFieldNames}{$nFeature} } },
+            $_[0]->{_geneTrackRegionHref}{$_[2]}{$geneRef}{$cachedDbNames->{$nFeature}};
+            #$self->{_geneTrackRegionHref}{$chr}{$geneRef}{$cachedDbNames->{$nFeature}};
           }
       }
-    }# else { $self->log('warn', "no " . $self->name . " or " . $nearestSubTrackName . " found"); }
+    }
   }
 
-  if($txNumbers && $self->join) {
+  #Reads:       && $self->join) {
+  if($txNumbers && $_[0]->join) {
     for my $txNumber(ref $txNumbers ? @$txNumbers : $txNumbers) {
-      #the features specified in the region database which we want for nearest gene records
-      for my $fName ( @{$self->joinTrackFeatures} ) {
-        push @{$out{ $self->{_allJoinFieldNames}{$fName} } },
-         $self->{_geneTrackRegionHref}{$chr}{$txNumber}{$cachedDbNames->{$fName} };
+      #The features specified in the region database which we want for nearest gene records
+      #Reads:         @{$self->{_joinTracksFeatures} }
+      for my $fName ( @{$_[0]->{_joinTrackFeatures}} ) {
+        #Reads       $self->{_allJoinFieldNames}{$fName} } },
+        push @{$out{ $_[0]->{_allJoinFieldNames}{$fName} } },
+        #Reads
+        #$self->{_geneTrackRegionHref}{$chr}{$txNumber}{$cachedDbNames->{$fName} };
+         $_[0]->{_geneTrackRegionHref}{$_[2]}{$txNumber}{$cachedDbNames->{$fName} };
       }
     }
   }
 
   if( !$txNumbers ) {
-    $out{$self->{_siteTypeKey}} = $intergenic;
+    #Reads:
+    #$out{$self->{_siteTypeKey}} = $intergenic;
+    $out{$_[0]->{_siteTypeKey}} = $intergenic;
     return \%out;
   }
 
   ################## Populate site information ########################
   # save unpacked sites, for use in txEffectsKey population #####
-  #moose attrs are very slow, cache
-  
+  # moose attrs are very slow, cache
 
   my $hasCodon;
   OUTER: for my $site ($multiple ? @$siteData : $siteData) {
     for (my $i = 0; $i < @$site; $i++) {
       if($i == $codonPositionIdx){
         # We store codon position as 0-based, but people probably expect 1-based
-        push @{ $out{$self->{_keysMap}->{$i} } }, $site->[$i] + 1;
+        #Reads:      $self->{_keysMap}{$i}
+        push @{ $out{$_[0]->{_keysMap}{$i}} }, $site->[$i] + 1;
       } else {
-        push @{ $out{$self->{_keysMap}->{$i} } }, $site->[$i];
+        #Reads:      $self->{_keysMap}{$i}
+        push @{ $out{$_[0]->{_keysMap}{$i} } }, $site->[$i];
       }
     }
 
     #### Populate refAminoAcidKey; note that for a single site
     ###    we can have only one codon sequence, so not need to set array of them ###
     if( defined $site->[$codonSequenceIdx] && length $site->[$codonSequenceIdx] == 3) {
-      push @{ $out{$self->{_refAminoAcidKey}} }, $codonMap->codon2aa( $site->[$codonSequenceIdx] );
+      #Reads:      $self->{_refAminoAcidKey}
+      push @{ $out{$_[0]->{_refAminoAcidKey}} }, $codonMap->codon2aa( $site->[$codonSequenceIdx] );
       $hasCodon = 1;
     }
   }
 
   # ################# Populate geneTrack's user-defined features #####################
-  foreach ($self->allFeatureNames) {
+  #Reads:            $self->{_features}
+  for my $feature (@{$_[0]->{_features}}) {
     INNER: for my $txNumber ($multiple ? @$txNumbers : $txNumbers) {
-      push @{ $out{$_} }, $self->{_geneTrackRegionHref}{$chr}{$txNumber}{ $cachedDbNames->{$_} };
+      #Reads:                   $self->{_geneTrackRegionHref}{$chr}{$txNumber}{ $cachedDbNames->{$feature} };
+      push @{ $out{$feature} }, $_[0]->{_geneTrackRegionHref}{$_[2]}{$txNumber}{ $cachedDbNames->{$feature} };
     }
   }
 
@@ -254,7 +292,8 @@ sub get {
   # #############  we may want to know how/if they disturb genes  #####################
   
   # Looping over string, int, or ref: https://ideone.com/4APtzt
-  TX_EFFECTS_LOOP: for my $allele (ref $allelesAref ? @$allelesAref : $allelesAref) {
+  #Reads:                          ref $allelesAref ? @$allelesAref : $allelesAref
+  TX_EFFECTS_LOOP: for my $allele (ref $_[5] ? @{$_[5]} : $_[5]) {
     my @accum;
 
     if(length($allele) > 1) {
@@ -262,7 +301,8 @@ sub get {
       my $type = substr($allele, 0, 1);
 
       #store as array because our output engine writes [ [one], [two] ] as "1,2"
-      push @accum, [ $self->_annotateIndel($chr, $dbPosition, $allele) ];
+      #Reads:      [ $self->_annotateIndel($chr, $dbPosition, $allele) ];
+      push @accum, [ $_[0]->_annotateIndel($_[2], $_[3], $allele) ];
 
       next TX_EFFECTS_LOOP;
     }
@@ -274,16 +314,19 @@ sub get {
     SNP_LOOP: for my $site ( $multiple ? @$siteData : $siteData ) {
       if(!defined $site->[ $codonPositionIdx ]){
         push @accum, undef;
-        push @{ $out{$self->{_newAminoAcidKey}} }, undef;
+        #Reads: $out{$self->{_newAminoAcidKey}} }, undef;
+        push @{ $out{$_[0]->{_newAminoAcidKey}} }, undef;
 
         next SNP_LOOP;
       }
 
-      my $refCodonSequence = $out{ $self->{_codonSequenceKey} }[$i];
+      #Reads:                $out{ $self->{_codonSequenceKey} }[$i];
+      my $refCodonSequence = $out{ $_[0]->{_codonSequenceKey} }[$i];
 
       if(length($refCodonSequence) != 3) {
         push @accum, $truncated;
-        push @{ $out{$self->{_newAminoAcidKey}} }, undef;
+        #Reads: $out{$self->{_newAminoAcidKey}} }, undef;
+        push @{ $out{$_[0]->{_newAminoAcidKey}} }, undef;
         
         next SNP_LOOP;
       }
@@ -298,18 +341,23 @@ sub get {
 
       substr($alleleCodonSequence, $site->[ $codonPositionIdx ], 1 ) = $allele;
 
-      push @{ $out{$self->{_newCodonKey}} }, $alleleCodonSequence;
-      push @{ $out{$self->{_newAminoAcidKey}} }, $codonMap->codon2aa($alleleCodonSequence);
+      #Reads: $out{$self->{_newCodonKey}} }, $alleleCodonSequence;
+      push @{ $out{$_[0]->{_newCodonKey}} }, $alleleCodonSequence;
+      #Reads: $out{$self->{_newAminoAcidKey}} }, $codonMap->codon2aa($alleleCodonSequence);
+      push @{ $out{$_[0]->{_newAminoAcidKey}} }, $codonMap->codon2aa($alleleCodonSequence);
 
-      if(!defined $out{$self->{_newAminoAcidKey}}->[$i]) {
+      #Reads:      $out{$self->{_newAminoAcidKey}}->[$i]) {
+      if(!defined $out{$_[0]->{_newAminoAcidKey}}->[$i]) {
         $i++;
         next;
       }
 
       # If reference codon is same as the allele-substititued version, it's a Silent site
-      if( $codonMap->codon2aa($refCodonSequence) eq $out{$self->{_newAminoAcidKey}}->[$i] ) {
+      # Reads:                                      $out{$self->{_newAminoAcidKey}}->[$i] ) {
+      if( $codonMap->codon2aa($refCodonSequence) eq $out{$_[0]->{_newAminoAcidKey}}->[$i] ) {
         push @accum, $silent;
-      } elsif($out{$self->{_newAminoAcidKey}}->[$i] eq '*') {
+      #Reads: $out{$self->{_newAminoAcidKey}}->[$i] eq '*') {
+      } elsif($out{$_[0]->{_newAminoAcidKey}}->[$i] eq '*') {
         push @accum, $stopGain;
       } else {
         push @accum, $replacement;
@@ -319,7 +367,8 @@ sub get {
     }
 
     if(@accum) {
-      push @{ $out{ $self->{_exonicAlleleFunctionKey} } }, @accum > 1 ? \@accum : $accum[0];
+      #Reads:     $self->{_exonicAlleleFunctionKey}}}, @accum > 1 ? \@accum : $accum[0];
+      push @{$out{$_[0]->{_exonicAlleleFunctionKey}}}, @accum > 1 ? \@accum : $accum[0];
     }
   }
 
@@ -328,48 +377,77 @@ sub get {
 
 # TODO: remove the "NA"
 sub _annotateIndel {
-  my ($self, $chr, $dbPosition, $allele) = @_;
+  #To speed up this function call, avoid assigning these strings
+  #my ($self, $chr, $dbPosition, $allele) = @_;
+  #    $_[0], $_[1]. $_[2],      $_[3]
 
   my $beginning = '';
   my $middle = '';
 
   my $dbDataAref;
 
-  my $type = substr($allele, 0, 1);
+  #Reads:           $allele,   0, 1);
+  my $type = substr($_[3], 0, 1);
   #### Check if insertion or deletion ###
-  if($type eq '+') {
-    $beginning = (length( substr($allele, 1) ) % 3 ? $frameshift : $inFrame) . "[";
 
-    # This makes it easier to use a single string building function below.
-    $dbDataAref = [ $dbPosition + 1 ];
-    #by passing the dbRead function an array, we get an array of data back
+  ##################### If the site is an insertion ############################
+  #If it's an insetion, we only get the next upstream position
+  #It is difficult to predict the effects of an insertion, so for now we've just
+  #elected to take the annotaiton of the next upstream site, and then also check
+  #whether the insertion is a frameshift
+  if($type eq '+') {
+    #Reads                substr($allele, 1) ) % 3
+    $beginning = (length( substr($_[3], 1) ) % 3 ? $frameshift : $inFrame) . "[";
+
+    # Using an array makes it easier to use a single string building function below.
+
+    #Reads:       [ $dbPosition + 1 ];
+    $dbDataAref = [ $_[2] + 1 ];
+
+    #By passing the dbRead function an array, we get an array of data back
     #even if it's one position worth of data
-    $self->{_db}->dbRead( $chr, $dbDataAref );
+    #Reads:
+    #$self->{_db}->dbRead( $chr, $dbDataAref );
+    $_[0]->{_db}->dbRead( $_[1], $dbDataAref );
+
+  ###################### If the site is a deletion ######################
+  # Deletions are easier to understand; the annotation is the sum of all of the
+  # deleted bases. So we take all of the annotaiton
   } elsif($type eq '-') {
-    $beginning = ($allele % 3 ? $frameshift : $inFrame) . "[";
+    #Reads:       $allele % 3
+    $beginning = ($_[3] % 3 ? $frameshift : $inFrame) . "[";
     
-    #get everything including the current dbPosition, in order to simplify code
-    #small perf hit because few indels
-    $dbDataAref = [ $dbPosition + $allele .. $dbPosition ];
+    #Get everything including the current dbPosition, in order to simplify code
+    #Has a small perf. when there are few indels 
+    #Since $allele is a negative number
+    #Therefore $dbPosition + $allele == $dbPosition -N bases
+
+    #Reads:       [ $dbPosition + $allele .. $dbPosition ];
+    $dbDataAref = [ $_[2] + $_[3] .. $_[2] ];
 
     # dbRead modifies by reference; each position in dbDataAref gets database data or undef
     # if nothing found
-    $self->{_db}->dbRead( $chr, $dbDataAref );
+    #Reads:
+    #$self->{_db}->dbRead( $chr, $dbDataAref );
+    $_[0]->{_db}->dbRead( $_[1], $dbDataAref );
   } else {
-    $self->log("warn", "Can't recognize allele $allele on $chr:@{[$dbPosition + 1]}
+    #Reads:
+    #$self->log("warn"
+    $_[0]->log("warn", "Can't recognize allele $_[3] on $_[1]:@{[$_[2] + 1]}
       as valid indel (must start with - or +)");
     return undef;
   }
 
   # Will always be an array of dbData, which is to say an array of array presently
   for my $data (@$dbDataAref) {
-    if (! defined $data->[$self->dbName] ) {
+    #Reads:       $data->[$self->dbName] ) {
+    if (! defined $data->[$_[0]->dbName] ) {
       #this position doesn't have a gene track, so skip
       $middle .= "$intergenic,";
       next;
     }
-
-    my $siteData = $siteUnpacker->unpack($data->[$self->dbName]);
+    #Reads:        $siteUnpacker->unpack($data->[$self->dbName]);
+    my $siteData = $siteUnpacker->unpack($data->[$_[0]->dbName]);
 
     # If this position covers multiple transcripts, $siteData will be an array of arrays
       # and if not, it will be a 1D array of scalars
